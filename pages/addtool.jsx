5e37b3b85ff433ac1b2c0294e0b2d6094a1d4c1b
ailetter <ailetter.substack@gmail.com>
2023-05-25 16:06:21 +0530

    modified addtool page for protection

--- pages/addtool.jsx
+++ pages/addtool.jsx
@@ -1,25 +1,26 @@
-import { useEffect } from 'react';
-import db from '../firebase'
-import { writeBatch, doc } from "firebase/firestore";
-import aitools from '../ailetter database'
+// import { useEffect } from 'react';
+// import db from '../firebase'
+// import { writeBatch, doc } from "firebase/firestore";
+// import aitools from '../ailetter database'
 
 const AddTool = () => {
 
-    const batch = writeBatch(db);
+    // const batch = writeBatch(db);
 
-    const add2000tools = async () => {
-        for (const tool of aitools) {
-            const docRef = doc(db, "tools", tool.title);
-            batch.update(docRef, { "timestamp": 1684914083949 });
-            console.log(tool.index)
-        }
+    // const add2000tools = async () => {
+    //     for (const tool of aitools) {
+    //         const docRef = doc(db, "tools", tool.title);
+    //         batch.update(docRef, { "timestamp": 1684914083949 });
+    //         console.log(tool.index)
+    //     }
 
-        await batch.commit();
-    }
+    //     await batch.commit();
+    // }
 
     return (
         <div className='h-[50vh] flex items-center justify-center'>
-            <button onClick={add2000tools} className='bg-orange-400 p-3 hover:bg-transparent hover:outline-dashed'>add tool</button>
+            {/* <button onClick={add2000tools} className='bg-orange-400 p-3 hover:bg-transparent hover:outline-dashed'>add tool</button> */}
+            YOU ARE NOT SUPPOSED TO BE HERE
         </div>
     )
 }
